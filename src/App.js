6df39769c8c9046343b543415fe2fd9947887b85
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-04-08 04:49:45 +0500

    Add all components in app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ArrayDataType from './components/useStateHook/ArrayDataType';
+import BooleanDataType from './components/useStateHook/BooleanDataType';
+import NumberDataType from './components/useStateHook/NumberDataType';
+import ObjectDataType from './components/useStateHook/ObjectDataType';
+import StringDataType from './components/useStateHook/StringDataType';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Boolean Data Type</h2>
+      <BooleanDataType />
+      <hr />
+      <h2>Number Data Type</h2>
+      <NumberDataType />
+      <hr />
+      <h2>String Data Type</h2>
+      <StringDataType />
+      <hr />
+      <h2>Array Data Type</h2>
+      <ArrayDataType />
+      <hr />
+      <h2>Object Data Type</h2>
+      <ObjectDataType />
     </div>
   );
 }
